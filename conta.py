efdef2e943db3c93b212493ae0d27539a8282bbd
erick.melo <erick.melo@sorocaba.sp.gov.br>
2022-11-29 17:54:33 -0300

    Subindo o Curso de Orientação a Objetos em Python

---
+++ conta.py
@@ -0,0 +1,48 @@
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor):
+        return valor <= self.__saldo + self.__limite
+
+    def saca(self, valor):
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+            print(f'Sacado: R$ {valor}. Saldo da conta: R$ {self.__saldo}')
+        else:
+            print('Saldo insuficiente!')
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def get_titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_bancario():
+        return '001'
